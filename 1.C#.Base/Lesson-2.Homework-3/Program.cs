e5a9256310f38f39997e23a37a649977649575e7
Orlfi <orlfi@mail.ru>
2021-03-20 22:17:00 +0300

    lesson8 homework

--- 1.C#.Base/Lesson-2.Homework-3/Program.cs
+++ 1.C#.Base/Lesson-2.Homework-3/Program.cs
@@ -10,6 +10,9 @@ namespace Lesson_2.Homework_3
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+#if DEBUG
+
+#endif
             Console.Write("Введите число: ");
             if (int.TryParse(Console.ReadLine(), out int userNumber))
                 Console.Write(userNumber % 2 == 0 ? "четное" : "нечетное");
---
+++ 1.C#.Base/Lesson-8.Homework-1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Resources;
+using System.Configuration;
+
+namespace Lesson_8.Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ResourceManager rm = new ResourceManager("Lesson_8.Homework_1.Properties.TextResources", typeof(Program).Assembly);
+
+            var cm = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = cm.AppSettings.Settings;
+
+            if (settings["Name"] == null)
+            {
+                Console.Write(rm.GetString("NamePrompt") + ": ");
+                string name = Console.ReadLine();
+                settings.Add(new KeyValueConfigurationElement("Name", name));
+            }
+            else
+                Console.WriteLine(rm.GetString("Greeting"), settings["Name"].Value);
+
+            if (settings["Age"] == null)
+            {
+                Console.Write(rm.GetString("AgePrompt") + ": ");
+                string name = Console.ReadLine();
+                settings.Add(new KeyValueConfigurationElement("Age", name));
+            }
+            else
+                Console.WriteLine(rm.GetString("AgePrefix") + ": ", settings["Age"].Value);
+
+            if (settings["Business"] == null)
+            {
+                Console.Write(rm.GetString("BusinessPrompt") + ": ");
+                string name = Console.ReadLine();
+                settings.Add(new KeyValueConfigurationElement("Business", name));
+            }
+            else
+                Console.WriteLine(rm.GetString("BusinessPrefix") + ": ", settings["Business"].Value);
+
+            cm.Save(ConfigurationSaveMode.Modified);
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+
+}
