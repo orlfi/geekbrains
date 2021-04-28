ad9436b7524f792b09070ea015c0573f4f4fcbe4
Orlfi <orlfi@mail.ru>
2021-04-28 19:58:36 +0300

    add homework for lesson-4

---
+++ 2.Algorithms/Lesson-4/Homework-1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using static System.Console;
+
+namespace Homework_1
+{
+    class Program
+    {
+        const int SIZE = 10_000_000;
+        const int ITERATIONS = 10;
+        static string[] strings = new string[SIZE];
+        static HashSet<string> hashSet = new HashSet<string>(SIZE);
+        
+        static void Main(string[] args)
+        {
+            Fill();
+
+            for (int i = 1; i <= ITERATIONS; i++)
+            {
+                Random rnd = new Random();
+                string search = strings[rnd.Next(0, SIZE - 1)];
+                WriteLine($"Iteration N{i}:");
+                TestFindArray(search);
+                TestFindHashSet(search);
+                WriteLine();
+            }
+            ReadKey();
+        }
+
+        static void Fill()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < SIZE; i++)
+            {
+                string text = Guid.NewGuid().ToString();
+                strings[i] = text;
+                hashSet.Add(text);
+            }
+            sw.Stop();
+            WriteLine($"Fill time: {sw.ElapsedTicks / 10000.0}мс");
+        }
+
+        static void TestFindArray(string search)
+        {
+            int result;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            result = Array.IndexOf(strings, search);
+            sw.Stop();
+            if (result >=0)
+                WriteLine($"Array search time: {sw.ElapsedTicks/10000.0}мс");
+            else
+                WriteLine($"Not found in array!");
+        }
+
+        static void TestFindHashSet(string search)
+        {
+            bool result;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            result = hashSet.Contains(search);
+            sw.Stop();
+            if (result)
+                WriteLine($"HashSet search time: {sw.ElapsedTicks/10000.0}мс");
+            else
+                WriteLine($"Not found in HashSet !");
+        }
+    }
+}
---
+++ 2.Algorithms/Lesson-4/Homework-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Library;
+
+namespace TestBinnaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = { 16, 8, 4, 2, 1, 3, 6, 5, 7, 12, 10, 9, 11, 14, 13, 15, 24, 20, 18, 17, 19, 22, 21, 23, 28, 26, 25, 27, 30, 29, 31 };
+            BinnaryTree tree = new BinnaryTree();
+
+            foreach (var item in values)
+            {
+                tree.AddItem(item);
+            }
+
+            int heigth = tree.GetHeight();
+            Console.WriteLine($"\nTree heigth: {heigth}");
+
+            var node = tree.GetNodeByValue(28);
+            if (node != null)
+                Console.WriteLine($"\nFound an element with the value {node.Value}, Parent value: {node.Parent.Value}");
+
+            tree.PrintTree();
+
+            Console.WriteLine("\nRemove node (8), (20), (28)");
+            tree.RemoveItem(8);
+            tree.RemoveItem(20);
+            tree.RemoveItem(28);
+            tree.PrintTree();
+
+            Console.WriteLine("\nRemove branch (6)");
+            tree.RemoveBranch(6);
+            Console.WriteLine();
+            tree.PrintTree();
+            Console.ReadKey();
+        }
+    }
+}
