0464c4c164cb82a557ab8de940ed7a905b425851
Orlfi <orlfi@mail.ru>
2021-06-05 01:08:22 +0300

    add homework for lesson 1 ASP Net Core WEB API

---
+++ 3.AspNetCoreWebApi/Lesson-1/WeatherForecast/Controllers/WeatherForecastController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherForecast.Models;
+
+namespace WeatherForecast.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+        private Storage _storage;
+        public WeatherForecastController(Storage storage)
+        {
+            _storage = storage;
+        }
+
+        [HttpGet("read")]
+        public IActionResult Read([FromQuery] DateTime from, [FromQuery] DateTime to)
+        {
+            return Ok(_storage.Data.Where(item => item.Date >= from && item.Date <= to));
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromQuery] DateTime date, [FromQuery] int temperature)
+        {
+            if (temperature < -273)
+                return NotFound($"The temperature cannot be less than -273 in Celsius");
+
+            if (_storage.Data.Exists(item => item.Date == date))
+            {
+                return Conflict($"The temperature for the date {date} has already been created");
+            }
+            else
+            {
+                _storage.Data.Add(new DateTemperature(date, temperature));
+                return Ok();
+            }
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] DateTime date, [FromQuery] int temperature)
+        {
+            if (temperature < -273)
+                return NotFound($"The temperature cannot be less than -273 in Celsius");
+
+            var dateTemperature = _storage.Data.FirstOrDefault(item => item.Date == date);
+            if (dateTemperature == null)
+            {
+                return NotFound($"Update error! Temperature for date {date} not found");
+            }
+            else
+            {
+                dateTemperature.Temperature = temperature;
+                return Ok();
+            }
+
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] DateTime from, [FromQuery] DateTime to)
+        {
+            _storage.Data.RemoveAll(item => item.Date >= from && item.Date <= to);
+            return Ok();
+        }
+    }
+}
---
+++ 3.AspNetCoreWebApi/Lesson-1/WeatherForecast/Models/Storage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherForecast.Models
+{
+    public class Storage
+    {
+        const int INITIAL_COUNT = 100;
+
+        public List<DateTemperature> Data { get; set; }
+
+        public Storage()
+        {
+            Data = new List<DateTemperature>();
+            Init();
+        }
+
+        private void Init()
+        {
+            Random rnd = new Random();
+            int i =0;
+            while (i < INITIAL_COUNT)
+            {
+                DateTime dt = new DateTime(2021, 06, rnd.Next(1, 30), rnd.Next(0, 24), rnd.Next(0, 6)*10, 00);
+                if (!Data.Exists(item => item.Date == dt))
+                    Data.Add(new DateTemperature(dt, rnd.Next(5,25)));
+                i++;
+            }
+           
+        }
+
+    }
+}
