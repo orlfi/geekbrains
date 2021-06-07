cbacbbdde096c920c56d098462a3d2e12728696f
Orlfi <orlfi@mail.ru>
2021-06-07 23:26:33 +0300

    lesson2 homework

---
+++ 3.AspNetCoreWebApi/Lesson-2/Metrics/MetricsManager/Controllers/NetworkMetricsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/metrics/network")]
+    [ApiController]
+    public class NetworkMetricsController : ControllerBase
+    {
+        [HttpGet("agent/{agentId}/from/{from}/to/{to}")]
+        public IActionResult GetMetricsFromAgent([FromRoute] int agentId, [FromRoute] DateTime from, [FromRoute] DateTime to)
+        {
+            return Ok();
+        }
+
+        [HttpGet("cluster/from/{from}/to/{to}")]
+        public IActionResult GetMetricsFromAllCluster([FromRoute] DateTime from, DateTime to)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ 3.AspNetCoreWebApi/Lesson-2/Metrics/MetricsManager/Controllers/RamMetricsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/metrics/ram")]
+    [ApiController]
+    public class RamMetricsController : ControllerBase
+    {
+        [HttpGet("agent/{agentId}/from/{from}/to/{to}")]
+        public IActionResult GetMetricsFromAgent([FromRoute] int agentId, [FromRoute] DateTime from, [FromRoute] DateTime to)
+        {
+            return Ok();
+        }
+
+        [HttpGet("cluster/from/{from}/to/{to}")]
+        public IActionResult GetMetricsFromAllCluster([FromRoute] DateTime from, DateTime to)
+        {
+            return Ok();
+        }
+    }
+}
